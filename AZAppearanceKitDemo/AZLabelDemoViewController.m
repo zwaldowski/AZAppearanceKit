04f000ca64df1a0975fe4e00f0c4fc6c82206cdf
Zachary Waldowski <zwaldowski@gmail.com>
2012-06-27 12:58:36 -0400

    Fix label demo for recent changes.
    Signed-off-by: Zachary Waldowski <zwaldowski@gmail.com>

--- AZAppearanceKitDemo/AZLabelDemoViewController.m
+++ AZAppearanceKitDemo/AZLabelDemoViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "AZLabelDemoViewController.h"
+#import "AZLabel.h"
 
 @interface AZLabelDemoViewController ()
 
@@ -30,15 +31,27 @@
 {
     [super viewDidLoad];
 	
-	[self.label setShadowOffset: CGSizeMake(0, 1) blur: 0 color: [[UIColor whiteColor] colorWithAlphaComponent:0.75] forState: UIControlStateNormal];
-	[self.label setInnerShadowOffset: CGSizeMake(0, 1) blur: 2.0f color: [[UIColor blackColor] colorWithAlphaComponent:0.5] forState: UIControlStateNormal];
-	AZGradient *label1Gradient = [[AZGradient alloc] initWithColors: @[[UIColor colorWithRed:0.33 green:0.38 blue:0.47 alpha:1.0], [UIColor colorWithRed:0.41 green:0.47 blue:0.59 alpha:1.0]]];
-	[self.label setGradient: label1Gradient direction: AZGradientDirectionVertical forState: UIControlStateNormal];
+	[self.label setShadowOffset: CGSizeMake(0, 1) forState: UIControlStateNormal];
+	[self.label setShadowColor: [[UIColor whiteColor] colorWithAlphaComponent:0.75] forState: UIControlStateNormal];
+	[self.label setShadowBlur: 0 forState: UIControlStateNormal];
+	[self.label setInnerShadowOffset: CGSizeMake(0, 1) forState: UIControlStateNormal];
+	[self.label setInnerShadowColor: [[UIColor blackColor] colorWithAlphaComponent:0.5] forState: UIControlStateNormal];
+	[self.label setInnerShadowBlur: 2.0f forState: UIControlStateNormal];
 	
-	[self.label2 setShadowOffset: CGSizeMake(0, 1) blur: 0 color: [[UIColor whiteColor] colorWithAlphaComponent:0.75] forState: UIControlStateNormal];
-	[self.label2 setInnerShadowOffset: CGSizeMake(0, 1) blur: 2.0f color: [[UIColor blackColor] colorWithAlphaComponent:0.5] forState: UIControlStateNormal];
+	AZGradient *label1Gradient = [[AZGradient alloc] initWithColors: @[[UIColor colorWithRed:0.33 green:0.38 blue:0.47 alpha:1.0], [UIColor colorWithRed:0.41 green:0.47 blue:0.59 alpha:1.0]]];
+	[self.label setGradient: label1Gradient forState:UIControlStateNormal];
+	[self.label setGradientDirection: AZGradientDirectionVertical forState: UIControlStateNormal];
+		
+	[self.label2 setShadowOffset: CGSizeMake(0, 1) forState: UIControlStateNormal];
+	[self.label2 setShadowColor: [[UIColor whiteColor] colorWithAlphaComponent:0.75] forState: UIControlStateNormal];
+	[self.label2 setShadowBlur: 0 forState: UIControlStateNormal];
+	[self.label2 setInnerShadowOffset: CGSizeMake(0, 1) forState: UIControlStateNormal];
+	[self.label2 setInnerShadowColor: [[UIColor blackColor] colorWithAlphaComponent:0.5] forState: UIControlStateNormal];
+	[self.label2 setInnerShadowBlur: 2.0f forState: UIControlStateNormal];
+	
 	AZGradient *label2Gradient = [[AZGradient alloc] initWithColors: @[[UIColor colorWithRed:0.33 green:0.38 blue:0.47 alpha:1.0], [UIColor colorWithRed:0.41 green:0.47 blue:0.59 alpha:1.0]]];
-	[self.label2 setGradient: label2Gradient direction: AZGradientDirectionVertical forState: UIControlStateNormal];
+	[self.label2 setGradient: label2Gradient forState:UIControlStateNormal];
+	[self.label2 setGradientDirection: AZGradientDirectionVertical forState: UIControlStateNormal];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
