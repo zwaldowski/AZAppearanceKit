a63e9a55ec408917cb4544165bba3ccf57ce339e
Zachary Waldowski <zwaldowski@gmail.com>
2012-06-29 12:39:02 -0400

    Documentation for AZShadowedTableView.
    Signed-off-by: Zachary Waldowski <zwaldowski@gmail.com>

--- AZAppearanceKit/AZShadowedTableView.h
+++ AZAppearanceKit/AZShadowedTableView.h
@@ -7,6 +7,15 @@
 //  Copyright (c) 2011-2012 Alexsander Akers & Zachary Waldowski. All rights reserved.
 //
 
+/** `AZShadowedTableView` is a drop-in subclass of `UITableView`.
+ It draws four shadows into the table: two at the top and bottom
+ of the entire frame, and two above the first and last cells.
+ 
+ `AZShadowedTableView` is intended only for use with the plain table
+ view style, and will raise an exception if attempted otherwise.
+ 
+ `AZShadowedTableView` has no customization properties at this time.
+ */
 @interface AZShadowedTableView : UITableView
 
 @end
