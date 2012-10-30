f809f5a067dd485a51d158514000242fe7c9c2d9
Zachary Waldowski <zwaldowski@gmail.com>
2012-10-30 11:57:42 -0400

    Backwards-compatible implementation of NSShadow.
    Signed-off-by: Zachary Waldowski <zwaldowski@gmail.com>

---
+++ AZAppearanceKit/AZShadow.h
@@ -0,0 +1,39 @@
+//
+//  AZShadow.h
+//  AZAppearanceKit
+//
+//  Created by Zachary Waldowski on 10/29/12.
+//  Copyright (c) 2012 Alexsander Akers & Zachary Waldowski. All rights reserved.
+//
+
+#ifdef __IPHONE_OS_VERSION_MIN_REQUIRED
+
+#import <UIKit/UIKit.h>
+
+@protocol AZShadow <NSObject, NSCopying, NSCoding>
+
+@property (nonatomic, assign) CGSize shadowOffset;
+@property (nonatomic, assign) CGFloat shadowBlurRadius;
+@property (nonatomic, retain) id shadowColor;
+
++ (id)shadowWithOffset:(CGSize)shadowOffset blurRadius:(CGFloat)shadowBlurRadius;
++ (id)shadowWithOffset:(CGSize)shadowOffset blurRadius:(CGFloat)shadowBlurRadius color:(id)shadowColor;
+- (void)set;
+
+@end
+
+@interface NSShadow (AZShadow) <AZShadow>
+
+@end
+
+@interface AZShadow : NSObject <AZShadow>
+
+@end
+
+#else
+
+#import <Cocoa/Cocoa.h>
+
+@compatibility_alias AZShadow NSShadow;
+
+#endif
---
+++ AZAppearanceKit/AZShadow.m
@@ -0,0 +1,149 @@
+//
+//  AZShadow.m
+//  AZAppearanceKit
+//
+//  Created by Zachary Waldowski on 10/29/12.
+//  Copyright (c) 2012 Alexsander Akers & Zachary Waldowski. All rights reserved.
+//
+
+#import "AZShadow.h"
+
+#ifdef __IPHONE_OS_VERSION_MIN_REQUIRED
+
+@interface AZShadow ()
+
+@property (nonatomic, readonly, getter = az_hasShadowColor) BOOL hasShadowColor;
+
+@end
+
+static NSString *const AZShadowColorKey = @"NSShadowColor";
+static NSString *const AZShadowHorizKey = @"NSShadowHoriz";
+static NSString *const AZShadowVertKey = @"NSShadowVert";
+static NSString *const AZShadowBlurRadiusKey = @"NSShadowBlurRadius";
+
+@implementation AZShadow
+
+@synthesize shadowOffset = _shadowOffset, shadowBlurRadius = _shadowBlurRadius, shadowColor = _shadowColor;
+
++ (id)shadowWithOffset:(CGSize)shadowOffset blurRadius:(CGFloat)shadowBlurRadius {
+	return [self shadowWithOffset: shadowOffset blurRadius: shadowBlurRadius color: nil];
+}
+
++ (id)shadowWithOffset:(CGSize)shadowOffset blurRadius:(CGFloat)shadowBlurRadius color:(id)shadowColor {
+	id <AZShadow> ret = NSClassFromString(@"NSShadow") ? [NSClassFromString(@"NSShadow") new] : [AZShadow new];
+	ret.shadowOffset = shadowOffset;
+	ret.shadowBlurRadius = shadowBlurRadius;
+	ret.shadowColor = shadowColor;
+	return ret;
+}
+
+- (id)init {
+	if (NSClassFromString(@"NSShadow")) {
+		return (id)[[NSClassFromString(@"NSShadow") alloc] init];
+	}
+	return [super init];
+}
+
+- (id)copyWithZone:(NSZone *)zone {
+	id <AZShadow> ret = [[[self class] alloc] init];
+	ret.shadowOffset = self.shadowOffset;
+	ret.shadowBlurRadius = self.shadowBlurRadius;
+	ret.shadowColor = self.shadowColor;
+	return ret;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+	if ((self = [super init])) {
+		CGSize size;
+#if CGFLOAT_IS_DOUBLE
+		size.width = [aDecoder decodeDoubleForKey: AZShadowHorizKey];
+		size.height = [aDecoder decodeDoubleForKey: AZShadowVertKey];
+		self.shadowBlurRadius = [aDecoder decodeDoubleForKey: AZShadowBlurRadiusKey];
+#else
+		size.width = [aDecoder decodeFloatForKey: AZShadowHorizKey];
+		size.height = [aDecoder decodeFloatForKey: AZShadowVertKey];
+		self.shadowBlurRadius = [aDecoder decodeFloatForKey: AZShadowBlurRadiusKey];
+#endif
+		self.shadowOffset = size;
+		if ([aDecoder containsValueForKey: AZShadowColorKey]) {
+			self.shadowColor = [aDecoder decodeObjectForKey: AZShadowColorKey];
+		}
+	}
+	return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+#if CGFLOAT_IS_DOUBLE
+	[aCoder encodeDouble: self.shadowOffset.width forKey: AZShadowHorizKey];
+	[aCoder encodeDouble: self.shadowOffset.height forKey: AZShadowVertKey];
+	[aCoder encodeDouble: self.shadowBlurRadius forKey: AZShadowBlurRadiusKey];
+#else
+	[aCoder encodeFloat: self.shadowOffset.width forKey: AZShadowHorizKey];
+	[aCoder encodeFloat: self.shadowOffset.height forKey: AZShadowVertKey];
+	[aCoder encodeFloat: self.shadowBlurRadius forKey: AZShadowBlurRadiusKey];
+#endif
+	if (self.hasShadowColor) {
+		[aCoder encodeObject: self.shadowColor forKey: AZShadowColorKey];
+	}
+}
+
+- (BOOL)az_hasShadowColor {
+	return !!_shadowColor;
+}
+
+- (BOOL)isEqual:(id)object {
+	if (object == self) return YES;
+	if (![object isKindOfClass: [self class]]) return NO;
+	if ([object shadowBlurRadius] != self.shadowBlurRadius) return NO;
+	if (!CGSizeEqualToSize([object shadowOffset], self.shadowOffset)) return NO;
+	if (![[object shadowColor] isEqual: self.shadowColor]) return NO;
+	return YES;
+}
+
+- (NSString *)description {
+	NSMutableString *ret = [NSMutableString stringWithFormat: @"AZShadow %@", NSStringFromCGSize(self.shadowOffset)];
+	if (self.shadowBlurRadius)
+		[ret appendFormat: @" blur = %f", self.shadowBlurRadius];
+	if (self.hasShadowColor)
+		[ret appendFormat: @" color = {%@}", self.shadowColor];
+	return ret;
+}
+
+- (void)set {
+	CGContextRef ctx = UIGraphicsGetCurrentContext();
+	if (self.hasShadowColor)
+		CGContextSetShadowWithColor(ctx, self.shadowOffset, self.shadowBlurRadius, [self.shadowColor CGColor]);
+	else
+		CGContextSetShadow(ctx, self.shadowOffset, self.shadowBlurRadius);
+}
+
+@end
+
+@implementation NSShadow (AZShadow)
+
+@dynamic shadowBlurRadius, shadowColor, shadowOffset;
+
++ (id)shadowWithOffset:(CGSize)shadowOffset blurRadius:(CGFloat)shadowBlurRadius {
+	return [self shadowWithOffset: shadowOffset blurRadius: shadowBlurRadius color: nil];
+}
+
++ (id)shadowWithOffset:(CGSize)shadowOffset blurRadius:(CGFloat)shadowBlurRadius color:(id)shadowColor {
+	NSShadow *ret = [[[self class] alloc] init];
+	ret.shadowOffset = shadowOffset;
+	ret.shadowBlurRadius = shadowBlurRadius;
+	if (shadowColor) // setting to nil will cause a flag to be set
+		ret.shadowColor = shadowColor;
+	return ret;
+}
+
+- (void)set {
+	CGContextRef ctx = UIGraphicsGetCurrentContext();
+	if (self.shadowColor)
+		CGContextSetShadowWithColor(ctx, self.shadowOffset, self.shadowBlurRadius, [self.shadowColor CGColor]);
+	else
+		CGContextSetShadow(ctx, self.shadowOffset, self.shadowBlurRadius);
+}
+
+@end
+
+#endif
